bec23400411091738e5b1c917e368ecc61e04728
mitchellstasko <mstasko3@gatech.edu>
2018-09-19 20:38:54 -0400

    Welcome screen implemented for future purposes

--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/WelcomeScreen.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/WelcomeScreen.java
@@ -1,7 +1,13 @@
 package com.example.rahul.donationtrackerapp.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -10,4 +16,17 @@ public class WelcomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
     }
+
+    public void loginOnPressed(View view) {
+        Intent intent = new Intent(this, Login.class);
+        Button loginButton = (Button) findViewById(R.id.loginbutton);
+        startActivity(intent);
+    }
+
+    public void registerOnPressed(View view) {
+        Intent intent = new Intent(this, Register.class);
+        Button registerButton = (Button) findViewById(R.id.registerbutton);
+        startActivity(intent);
+    }
+
 }
