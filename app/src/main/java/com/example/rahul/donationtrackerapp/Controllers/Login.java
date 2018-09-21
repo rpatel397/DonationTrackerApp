a36f498edec40aac949196f1a8738b74b8cf6df7
Rakeshgore <42789046+Rakeshgore@users.noreply.github.com>
2018-09-21 08:18:47 -0400

    Final update

--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/Login.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/Login.java
@@ -1,22 +1,20 @@
 package com.example.rahul.donationtrackerapp.Controllers;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
-
 public class Login extends AppCompatActivity {
 
+    private  EditText username;
+    private  EditText password;
+    private  Button login_button;
 
-    private EditText username;
-    private EditText password;
-    private Button login_button;
 
 
     @Override
@@ -25,42 +23,30 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         LoginButton();
 
+
     }
+
 
     public void LoginButton() {
         username = findViewById(R.id.editText_username);
         password = findViewById(R.id.editText_password);
         login_button = findViewById(R.id.button_login);
 
-        login_button.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (username.getText().toString().equals("user") && password.getText().toString().equals("pass")) {
-                            {
-                                Toast.makeText(Login.this, "Username and password is correct",Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(Login.this, WelcomeScreen.class);
-                                startActivity(intent);
 
-
-                            }
-                        } else if ((username.getText().toString().equals("user") || password.getText().toString().equals("pass"))) {
-                            Toast.makeText(Login.this, "Username or password is NOT correct",
-                                    Toast.LENGTH_SHORT).show();
-
-                            Intent intent = new Intent(Login.this, WelcomeScreen.class);
-                            startActivity(intent);
-
-                        }
-                    }
-                }
-        );
     }
 
+    public void loginOnPressed(View view){
+        Intent intent = new Intent(Login.this, WelcomeScreen.class);
+        if (username.getText().toString().equals("user") && password.getText().toString().equals("pass")) {
+            Toast.makeText(Login.this,"Login Successful",Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }else {
+            Toast.makeText(Login.this,"Login Failed",Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }
 
 
 
-
-
+    }
 
 }
--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/WelcomeScreen.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/WelcomeScreen.java
@@ -1,7 +1,9 @@
 package com.example.rahul.donationtrackerapp.Controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 ;
 
 public class WelcomeScreen extends AppCompatActivity {
@@ -9,9 +11,15 @@ public class WelcomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome_screen);
+
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
 
 
 
     }
+
+
+
 }
