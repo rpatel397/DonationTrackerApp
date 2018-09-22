63a293046fa703b37c62302b02cd852e0873eb0c
Bailey Spencer <bspencer30@gatech.edu>
2018-09-21 20:32:47 -0400

    Compiled M4

--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/Login.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/Login.java
@@ -46,7 +46,7 @@ public class Login extends AppCompatActivity {
         Intent intent = new Intent(Login.this, WelcomeScreen.class);
         if (username.getText().toString().equals("user") && password.getText().toString().equals("pass")) {
             Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-            startActivity(intent);
+            startActivity(new Intent(Login.this, UserScreen.class));
         } else {
             Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
             password.setText("");
