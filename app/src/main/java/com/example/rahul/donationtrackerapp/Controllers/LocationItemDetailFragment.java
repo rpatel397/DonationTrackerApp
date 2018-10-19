c2100638a6fdc451a87105ca6f9e7666c57e4d27
mitchellstasko <mstasko3@gatech.edu>
2018-10-19 18:42:11 -0400

    Made model folder

--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/LocationItemDetailFragment.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/LocationItemDetailFragment.java
@@ -1,13 +1,13 @@
 package com.example.rahul.donationtrackerapp.Controllers;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.rahul.donationtrackerapp.Controllers.Model.SimpleModel;
 
 public class LocationItemDetailFragment extends Fragment {
 
--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/LocationList.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/LocationList.java
@@ -6,11 +6,12 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.rahul.donationtrackerapp.Controllers.Model.SimpleModel;
 
 import java.util.List;
 
