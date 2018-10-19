72c1fd5a3a78670415ff429b87f7fe5a286467bd
Bailey Spencer <bspencer30@gatech.edu>
2018-10-19 19:26:28 -0400

    added location to Model package

--- app/src/main/java/com/example/rahul/donationtrackerapp/Model/User.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Model/User.java
@@ -4,19 +4,21 @@ package com.example.rahul.donationtrackerapp.Model;
 import java.util.Arrays;
 import java.util.List;
 
+enum accountType {USER, LocationEmployee, Admin, Manager}
+
 public class User {
     public static List<String> legalAccounts = Arrays.asList("USER", "Location Employee", "Admin", "Manager");
-
     private String name;
     private String id;
     private String password;
-    private enum accountType;
+    private accountType permissions;
     private boolean accountState;
 
-    public User(String name, String id, String password, boolean accountState) {
+    public User(String name, String id, String password, boolean accountState, String permissions) {
         this.name = name;
         this.id = id;
         this.password = password;
         this.accountState = accountState;
+        this.permissions = accountType.valueOf(permissions);
     }
 }
