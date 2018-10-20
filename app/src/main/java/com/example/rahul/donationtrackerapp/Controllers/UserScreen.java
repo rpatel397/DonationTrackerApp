893e83c700b19f2bb0fffb73100ccfa334c84443
Bailey Spencer <bspencer30@gatech.edu>
2018-10-20 18:54:45 -0400

    firebase location

--- app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/UserScreen.java
+++ app/src/main/java/com/example/rahul/donationtrackerapp/Controllers/UserScreen.java
@@ -1,6 +1,7 @@
 package com.example.rahul.donationtrackerapp.Controllers;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,22 +10,39 @@ import android.widget.Toast;
 
 public class UserScreen extends AppCompatActivity {
 
+
+    private Button logOut_button;
+    private Button location_button;
+    private Button inventory_button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_screen);
+        logOut_button= findViewById(R.id.logoutButton);
+        location_button = findViewById(R.id.locationButton);
+        inventory_button = findViewById(R.id.button_Inventory);
+
     }
 
     public void logoutOnPressed(View view) {
         Intent intent = new Intent(this, WelcomeScreen.class);
-        Button loginButton = (Button) findViewById(R.id.logoutButton);
         startActivity(intent);
         finish();
     }
 
     public void locationListOnPressed(View view) {
         Intent intent = new Intent(this, LocationList.class);
-        Button locationButton = (Button) findViewById(R.id.locationButton);
+        setContentView(R.layout.activity_location_list);
         startActivity(intent);
     }
+
+    public void inventoryOnPressed(View view) {
+        Intent intent = new Intent(this, LocationInventory.class);
+        //setContentView(R.layout.activity_location_inventory);
+        startActivity(intent);
+    }
+
+
 }
