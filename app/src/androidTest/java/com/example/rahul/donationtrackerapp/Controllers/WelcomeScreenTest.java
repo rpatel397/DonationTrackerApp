5a43320f5e557a47b5d77f20448d8a893f767fb4
mitchellstasko <mstasko3@gatech.edu>
2018-11-14 22:39:59 -0500

    Delete extraneous stuff

--- app/src/androidTest/java/com/example/rahul/donationtrackerapp/Controllers/WelcomeScreenTest.java
+++ app/src/androidTest/java/com/example/rahul/donationtrackerapp/Controllers/WelcomeScreenTest.java
@@ -1,42 +1,25 @@
 package com.example.rahul.donationtrackerapp.Controllers;
 
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.SystemClock;
-import android.support.test.espresso.Espresso;
-
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Assert;
-import org.junit.Before;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import android.support.test.rule.ActivityTestRule;
 
-import androidx.test.espresso.NoMatchingViewException;
-import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
-import static android.support.test.espresso.matcher.ViewMatchers.hasImeAction;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.intent.Intents.intended;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.AllOf.allOf;
+
 
 
 @RunWith(AndroidJUnit4.class)
